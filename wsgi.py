6935d15db54112df89c569e9e517860af3d75df7
SamMadeAMistake <158320947+SamMadeAMistake@users.noreply.github.com>
2025-09-30 01:03:55 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,8 @@ from flask.cli import with_appcontext, AppGroup
 
 from App.database import db, get_migrate
 from App.models import User
+from App.models import student
+from App.models import staff
 from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users, initialize )
 
